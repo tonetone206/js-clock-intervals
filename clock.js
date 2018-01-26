3839e5f807aa428286777323253d4b627a740c63
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2018-01-25 22:45:13 -0800

    working clock w/out current time Anthony Pitts

---
+++ clock.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+var degrees = 0;
+ 
+var secondsDegrees = 0
+var second = document.getElementById("second");
+setInterval(function(){
+second.style.transform = "rotate(" + degrees + "deg)";
+degrees = degrees + 6;
+}, 1000);
+   
+ 
+var minutesDegrees = 0;
+var minute = document.getElementById("minute");
+setInterval(function(){
+minute.style.transform = "rotate(" + degrees + "deg)";
+degrees = degrees + 6;
+}, 60000);
+ 
+   
+var hoursDegrees = 0;
+var hour = document.getElementById("hour");
+setInterval(function(){
+hour.style.transform = "rotate(" + degrees + "deg)";
+degrees = degrees + 6;
+}, 3.6e+6);
+ 
+});
+ 
+ 
