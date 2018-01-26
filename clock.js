f4858298836dee494f21e8b027934eef9c7c1442
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2018-01-26 10:13:33 -0800

    js-clock-intervals

--- clock.js
+++ clock.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-   console.log("DOM fully loaded and parsed");
+   console.log("DOM fully loaded and parsed now");
 var degrees = 0;
+
+var hoursDegrees = 0;
+var hour = document.getElementById("hour");
+setInterval(function(){
+hour.style.transform = "rotate(" + degrees + "deg)";
+degrees = degrees + 30;
+}, 3600000);
+
+var minutesDegrees = 0;
+var minute = document.getElementById("minute");
+setInterval(function(){
+minute.style.transform = "rotate(" + degrees + "deg)";
+degrees = degrees + 6;
+}, 60000);
  
 var secondsDegrees = 0
 var second = document.getElementById("second");
@@ -9,22 +23,5 @@ second.style.transform = "rotate(" + degrees + "deg)";
 degrees = degrees + 6;
 }, 1000);
    
- 
-var minutesDegrees = 0;
-var minute = document.getElementById("minute");
-setInterval(function(){
-minute.style.transform = "rotate(" + degrees + "deg)";
-degrees = degrees + 6;
-}, 60000);
- 
-   
-var hoursDegrees = 0;
-var hour = document.getElementById("hour");
-setInterval(function(){
-hour.style.transform = "rotate(" + degrees + "deg)";
-degrees = degrees + 6;
-}, 3.6e+6);
- 
 });
- 
  
